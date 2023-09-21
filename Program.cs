acabbf7f62a5d9d8744ec6578c8d0f0e34967284
VeraOnishchenko <143957168+VeraOnishchenko@users.noreply.github.com>
2023-09-21 22:28:20 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,54 +1,62 @@
-﻿using System;
+﻿// *************************************************
+//* Практическая работа 5                          *
+//* Выполнила: Онищенко В.И., группа 2-ИСП         *
+//* Составить программу работы алгоритма ветвления *
+//**************************************************
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PR_4._0
+namespace PR_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "Практическая работа №4";
+            int S, D, X, C, N, T, m, k; // объявление переменных N,T - цены лимонда и мороженного, X - разница,
+                                     // m,k - кол-во порций лимонада и мороженного, S - общая сумма, D - кол-во денег
+            Console.WriteLine("Практическая работа №5");
+            Console.Write("Введите цену лимонада: ");
+            Console.Write("N = "); // ввод исходных данных
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите цену мороженного: ");
+            Console.Write("T = ");
+            T = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество порций лимонада: ");
+            Console.Write("m = ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество порций мороженного: ");
+            Console.Write("k = ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество ваших денег: ");
+            Console.Write("D = ");
+            D = Convert.ToInt32(Console.ReadLine());
 
-            double a, b, c, x; // объявление переменных
-            double v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16;
-
-            Console.WriteLine("Здравствуйте!");
-            
-            
-            Console.Write("Введите a = "); // ввод исходных данных
-            a = Convert.ToDouble(Console.ReadLine()); // явное приведение к типу double
-            Console.Write("Введите b = "); // ввод исходных данных
-            b = Convert.ToDouble(Console.ReadLine()); // явное приведение к типу double
-            Console.Write("Введите c = "); // ввод исходных данных
-            c = Convert.ToDouble(Console.ReadLine()); // явное приведение к типу double
+            S = N * m + T * k; // вычисление общей суммы покупок
+            X = D - S; // вычисление разницы
+            C = S - D; // сколько нужно доплатить
+           
 
 
-            // расчет значения выражения
-            v1 = 1 / a;
-            v2 = 1 / b + c;
-            v3 = v1 - v2;
-            v4 = v1 + v2;
-            v5 = v3 / v4;
-            v6 = Math.Pow(b, 2);
-            v7 = Math.Pow(c, 2);
-            v8 = Math.Pow(a, 2);
-            v9 = v6 + v7 - v8;
-            v10 = 2 * b * c;
-            v11 = v9 / v10;
-            v11 += 1;
-            v12 = v5 * v11;
-            v13 = a - b - c;
-            v14 = a * b * c;
-            v15 = v13 / v14;
 
-            x = v12 / v15;
+            // проверка условия
+            if (X > 0) // если X>0, то
+                Console.WriteLine("Возьмите сдачу " + X);
+            else if (X == 0) // иначе
+                Console.WriteLine("Спасибо за покупку!");
+            else 
+                Console.WriteLine("Нужно доплатить: " + C);
+            Console.ReadKey();
 
-            // вывод результата на экран
-            Console.WriteLine("Результат: x = {0: #.#####}", x);
-            Console.ReadKey(); // задержка экрана консоли
+
+
+
+
+
+
+
         }
     }
 }
